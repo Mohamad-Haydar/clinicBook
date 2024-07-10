caf918bebcf5a01fbd47e31ece969272a754a70c
Mohamad-Haydar <mfh313haydar@gmail.com>
2024-07-10 08:21:07 +0300

    change the design of the database
    write new sql scripts for creating and filling the new database desing

--- Data/dbo/StoredProcedures/sp_insert_doctor.sql
+++ Data/dbo/StoredProcedures/sp_insert_doctor.sql
@@ -3,7 +3,7 @@ CREATE OR REPLACE PROCEDURE sp_insert_doctor(
 	IN first_name character varying,
 	IN last_name character varying,
 	IN email character varying,
-	IN phone_number character varying
+	IN phone_number character varying,
 	IN description text,
 	IN category_id int)
 LANGUAGE plpgsql
@@ -12,4 +12,4 @@ BEGIN
 	INSERT INTO doctor(first_name, last_name, email, phone_number, description, category_id)
 	VALUES($1, $2, $3, $4, $5, $6);
 END;
-$$ LANGUAGE plpgsql;
+$$;
