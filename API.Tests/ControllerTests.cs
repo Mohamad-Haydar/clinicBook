91358c01f91e5b00baab390b36e06773ffb09bc3
Mohamad-Haydar <mfh313haydar@gmail.com>
2024-07-19 14:47:17 +0300

    Set Up for start unit testing

---
+++ API.Tests/ControllerTests.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+
+namespace API.Tests
+{
+    public class ControllerTests
+    {
+        [Theory]
+        [InlineData(1,2,3)]
+        [InlineData(2,3,5)]
+        [InlineData(12.75,13.75,26.5)]
+        public void  Test_Testing(double x, double y, double expected)
+        {
+            // Arrange
+
+            // Act
+            double actual = x + y;
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
